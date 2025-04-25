e9abba2ed9bfca416085009ffa050b414547fadc
Tomonori Ishizaka <xishizaka.tomonori@contract.dentsusoken.com>
2025-04-25 16:46:14 +0900

    Fix CS Fixer

--- app/Http/Controllers/Test/MailController.php
+++ app/Http/Controllers/Test/MailController.php
@@ -1,11 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers\Test;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Mail;
 
 class MailController extends Controller
 {
