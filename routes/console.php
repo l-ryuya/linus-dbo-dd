81f91b2a147ccc84eb000bcee2ecaed7e784b12a
Tomonori Ishizaka <xishizaka.tomonori@contract.dentsusoken.com>
2025-08-13 20:36:52 +0900

    Change schedule:workをシェル経由ではなく直接起動に変更

--- routes/console.php
+++ routes/console.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Schedule;
 
+Schedule::useCache('database');
+
 Schedule::call(function () {
-    Log::channel('stdout')->info("task is running");
-})->hourly();
+    Log::channel('stdout')->info("schedule:work is running");
+})
+    ->name('log-schedule-work')
+    ->everyFifteenMinutes()
+    ->onOneServer()
+    ->withoutOverlapping();
