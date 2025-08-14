b367b5d3548cdf00ccd3eda08ac8339a7b5c44aa
shimoji <shimoji@linus.co.jp>
2025-08-14 11:56:59 +0900

    以前の調査時のコメントアウトを解除する

--- routes/endpoints/tenant/service_contract.php
+++ routes/endpoints/tenant/service_contract.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 use App\Http\Controllers\Tenant\ServiceContractController;
 
-// Route::prefix('tenant')->middleware(['auth', 'roles:admin,tenant'])->group(function () {
+Route::prefix('tenant')->middleware(['auth', 'roles:admin,tenant'])->group(function () {
     Route::get('/service-contracts', [ServiceContractController::class, 'index']);
     Route::get('/service-contracts/{public_id}', [ServiceContractController::class, 'show']);
-// });
+});
 
-// Route::prefix('tenant')->middleware(['auth', 'roles:tenant'])->group(function () {
+Route::prefix('tenant')->middleware(['auth', 'roles:tenant'])->group(function () {
     Route::post('/service-contracts', [ServiceContractController::class, 'store']);
     Route::post('/service-contracts/draft', [ServiceContractController::class, 'storeDraft']);
     Route::put('/service-contracts/{public_id}', [ServiceContractController::class, 'update']);
-// });
+});
